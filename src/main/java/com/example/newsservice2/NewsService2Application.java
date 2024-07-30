c8def780d9e53a0e11ccf1df65d351c392248d11
Евгений Сапрунов <sapra10062018@gmail.com>
2024-07-30 11:45:15 +0300

    push commit

--- src/main/java/com/example/newsservice2/NewsService2Application.java
+++ src/main/java/com/example/newsservice2/NewsService2Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class NewsService2Application {
 
     public static void main(String[] args) {
+
         SpringApplication.run(NewsService2Application.class, args);
     }
 
