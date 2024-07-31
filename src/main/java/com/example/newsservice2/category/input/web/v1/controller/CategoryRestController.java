6932a14f436b42d2e4f7d8026d45d28558dc2c62
Евгений Сапрунов <sapra10062018@gmail.com>
2024-07-31 15:19:41 +0300

    category package initial commit.

---
+++ src/main/java/com/example/newsservice2/category/input/web/v1/controller/CategoryRestController.java
@@ -0,0 +1,11 @@
+package com.example.newsservice2.category.input.web.v1.controller;
+
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/controllers")
+@Tag(name = "CONTROLLERS")
+public class CategoryRestController {
+}
