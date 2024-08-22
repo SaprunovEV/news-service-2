09392224214e8e70e9b5b6807d3687d2f4fcffaf
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 14:19:24 +0300

    created test to check saving new category.

--- src/test/java/com/example/newsservice2/category/input/web/v1/controller/CategoryRestControllerTest.java
+++ src/test/java/com/example/newsservice2/category/input/web/v1/controller/CategoryRestControllerTest.java
@@ -2,10 +2,12 @@ package com.example.newsservice2.category.input.web.v1.controller;
 
 import com.example.newsservice2.category.input.web.v1.model.CategoryFilter;
 import com.example.newsservice2.config.AbstractIntegrationTest;
+import com.example.newsservice2.testUtils.CategoryPayloadTestDataBuilder;
 import com.example.newsservice2.testUtils.StringTestUtils;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import jakarta.transaction.Transactional;
 import net.javacrumbs.jsonunit.JsonAssert;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -13,28 +15,25 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTypeExcludeFilter;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
 import java.util.stream.Stream;
 
 import static com.example.newsservice2.category.input.web.v1.controller.testBuilder.CategoryTestDataBuilder.aCategory;
+import static com.example.newsservice2.testUtils.CategoryPayloadTestDataBuilder.aCategoryPayload;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @TypeExcludeFilters(DataJpaTypeExcludeFilter.class)
 @Transactional
 @AutoConfigureCache
-@AutoConfigureDataJpa
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@AutoConfigureTestEntityManager
 @ImportAutoConfiguration
 @AutoConfigureMockMvc
 @Testcontainers
@@ -77,5 +76,22 @@ class CategoryRestControllerTest extends AbstractIntegrationTest {
         String expected = StringTestUtils.readStringFromResources("/responses/web/v1/find_by_filter_"+ filter.getPageNumber() +"_response.json");
 
         JsonAssert.assertJsonEquals(expected, actual, JsonAssert.whenIgnoringPaths("categories[*].id"));
+    }
+
+    @Test
+    void whenCreateNewCategory_thenReturnCategoryWithId_andSaveCategoryToDatabase() throws Exception {
+        CategoryPayloadTestDataBuilder testDataBuilder = aCategoryPayload();
+
+        String actual = mvc.perform(
+                        post("/api/v1/category")
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(objectMapper.writeValueAsString(testDataBuilder.build())))
+                .andExpect(status().isCreated())
+                .andReturn().getResponse()
+                .getContentAsString();
+
+        String expected = StringTestUtils.readStringFromResources("/responses/web/v1/create_new_category_response.json");
+
+        JsonAssert.assertJsonEquals(expected, actual, JsonAssert.whenIgnoringPaths("id"));
     }
 }
