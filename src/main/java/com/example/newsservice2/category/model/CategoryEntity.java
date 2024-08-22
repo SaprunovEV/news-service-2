03059cab92c15e316ce750b782ebd751854c913f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 14:14:11 +0300

    Use lombok.

--- src/main/java/com/example/newsservice2/category/model/CategoryEntity.java
+++ src/main/java/com/example/newsservice2/category/model/CategoryEntity.java
@@ -1,29 +1,17 @@
 package com.example.newsservice2.category.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "category")
+@Getter
+@Setter
 public class CategoryEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     private String name;
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
