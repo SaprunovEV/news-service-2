559625875ed5c3447c8a5965a7a27cd6aed72cf6
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 17:22:14 +0300

    implemented a method that takes out a page with categories

--- src/main/java/com/example/newsservice2/category/service/DatabaseCategoryService.java
+++ src/main/java/com/example/newsservice2/category/service/DatabaseCategoryService.java
@@ -3,15 +3,22 @@ package com.example.newsservice2.category.service;
 import com.example.newsservice2.category.input.web.v1.model.CategoryFilter;
 import com.example.newsservice2.category.input.web.v1.model.CategoryPayload;
 import com.example.newsservice2.category.model.CategoryEntity;
+import com.example.newsservice2.category.output.repository.specification.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service
+@RequiredArgsConstructor
 public class DatabaseCategoryService implements CategoryService {
+    private final CategoryRepository repository;
     @Override
     public List<CategoryEntity> getByFilter(CategoryFilter filter) {
-        return null;
+        Page<CategoryEntity> page = repository.findAll(PageRequest.of(filter.getPageNumber(), filter.getPageSize()));
+        return page.getContent();
     }
 
     @Override
