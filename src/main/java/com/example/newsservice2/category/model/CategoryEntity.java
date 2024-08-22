de15e03d6536b9afef78989dd4b22102490b0e9b
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 14:13:22 +0300

    added columns to Category table

--- src/main/java/com/example/newsservice2/category/model/CategoryEntity.java
+++ src/main/java/com/example/newsservice2/category/model/CategoryEntity.java
@@ -1,4 +1,29 @@
 package com.example.newsservice2.category.model;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "category")
 public class CategoryEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
 }
