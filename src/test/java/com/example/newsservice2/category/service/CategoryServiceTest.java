6af7e299d01e56ef5bf864f99811a32e8d93d634
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 16:53:54 +0300

    a test has been created to check the search by filter

--- src/test/java/com/example/newsservice2/category/service/CategoryServiceTest.java
+++ src/test/java/com/example/newsservice2/category/service/CategoryServiceTest.java
@@ -1,20 +1,46 @@
 package com.example.newsservice2.category.service;
 
+import com.example.newsservice2.category.input.web.v1.model.CategoryFilter;
+import com.example.newsservice2.category.model.CategoryEntity;
 import com.example.newsservice2.config.AbstractIntegrationTest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.ContextConfiguration;
 
+import java.util.List;
+
+import static com.example.newsservice2.category.input.web.v1.controller.testBuilder.CategoryTestDataBuilder.aCategory;
 import static org.junit.jupiter.api.Assertions.*;
 @DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@AutoConfigureTestEntityManager
 @ContextConfiguration(classes = CategoryServiceConf.class)
 class CategoryServiceTest extends AbstractIntegrationTest {
     @Autowired
     private CategoryService service;
 
     @Test
-    void shouldDoSomething() throws Exception {
+    void whenGetByFilter_thenReturnAllCategories() throws Exception {
+        getFacade().save(aCategory().withName("test1"));
+        getFacade().save(aCategory().withName("test2"));
+        getFacade().save(aCategory().withName("test3"));
+        getFacade().save(aCategory().withName("test4"));
+        getFacade().save(aCategory().withName("test5"));
+        getFacade().save(aCategory().withName("test6"));
+        getFacade().save(aCategory().withName("test7"));
 
+        CategoryFilter filter = new CategoryFilter();
+        filter.setPageSize(3);
+        filter.setPageNumber(0);
+        List<CategoryEntity> actual = service.getByFilter(filter);
+
+        assertAll(() -> {
+            assertNotNull(actual);
+            assertFalse(actual.isEmpty());
+            assertEquals(3, actual.size());
+        });
     }
 }
