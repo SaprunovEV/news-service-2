0b0fbc65be6b5f22dc4bdfdf6847bc60d84f2d22
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-22 15:16:31 +0300

    the basic logic of the category controller is implemented

--- src/main/java/com/example/newsservice2/category/input/web/v1/controller/CategoryRestController.java
+++ src/main/java/com/example/newsservice2/category/input/web/v1/controller/CategoryRestController.java
@@ -1,18 +1,15 @@
 package com.example.newsservice2.category.input.web.v1.controller;
 
-import com.example.newsservice2.category.input.web.v1.model.CategoryFilter;
-import com.example.newsservice2.category.input.web.v1.model.CategoryListResponse;
+import com.example.newsservice2.category.input.web.v1.model.*;
 import com.example.newsservice2.category.mapper.CategoryMapper;
 import com.example.newsservice2.category.service.CategoryService;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/v1/category")
@@ -23,9 +20,30 @@ public class CategoryRestController {
     private final CategoryService service;
     private final CategoryMapper mapper;
     @GetMapping()
-    public ResponseEntity<CategoryListResponse> findAllCategory(@RequestParam CategoryFilter filter) {
+    public ResponseEntity<CategoryListResponse> findAllCategory(@RequestParam @Valid CategoryFilter filter) {
         return ResponseEntity.status(HttpStatus.OK).body(
                 mapper.entityListToResponseList(service.getByFilter(filter))
         );
     }
+
+    @PostMapping
+    public ResponseEntity<CategoryResponse> createCategory(@RequestBody @Valid CategoryPayload payload) {
+        return ResponseEntity.status(HttpStatus.CREATED).body(
+                mapper.entityToResponse(service.createNewCategory(payload))
+        );
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<CategoryResponse> updateCategory(
+            @Valid CategoryId id,
+            @RequestBody @Valid CategoryPayload payload) {
+        return ResponseEntity.ok(mapper.entityToResponse(service.updateCategory(id.getId(), payload)));
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> deleteCategory(@Valid CategoryId id) {
+        service.deleteCategory(id.getId());
+        return ResponseEntity.noContent().build();
+    }
+
 }
--- src/main/java/com/example/newsservice2/category/input/web/v1/model/CategoryId.java
+++ src/main/java/com/example/newsservice2/category/input/web/v1/model/CategoryId.java
@@ -1,15 +1,12 @@
 package com.example.newsservice2.category.input.web.v1.model;
 
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@Builder
 @NoArgsConstructor
 @AllArgsConstructor
-public class CategoryResponse {
+public class CategoryId {
     private Long id;
-    private String name;
 }
