09a887d8fe56e5ce33c7e3da438f579f539bff70
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-23 14:28:33 +0300

    fixed a bug with incorrect validation annotation

--- src/main/java/com/example/newsservice2/category/input/web/v1/model/CategoryPayload.java
+++ src/main/java/com/example/newsservice2/category/input/web/v1/model/CategoryPayload.java
@@ -1,11 +1,11 @@
 package com.example.newsservice2.category.input.web.v1.model;
 
-import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Length;
 
 @Data
 @NoArgsConstructor
@@ -13,6 +13,6 @@ import lombok.NoArgsConstructor;
 public class CategoryPayload {
     @NotBlank(message = "Имя категории не должно быть пустое!")
     @NotNull(message = "Имя категории не должно быть null!")
-    @Min(value = 5, message = "Имя категории должно быть от 5 символов!")
+    @Length(min = 5, message = "Имя категории должно быть от 5 символов, В данный момент - {min}!")
     private String name;
 }
