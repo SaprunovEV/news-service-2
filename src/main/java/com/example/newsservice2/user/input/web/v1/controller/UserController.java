d0757003099ab07914a63dfbc5246375b33110fa
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-28 17:25:54 +0300

    Controller class has been created.

---
+++ src/main/java/com/example/newsservice2/user/input/web/v1/controller/UserController.java
@@ -0,0 +1,12 @@
+package com.example.newsservice2.user.input.web.v1.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RestControllerAdvice
+public class UserController {
+
+}
