8c898a8d64ccd90612d6bedb4e58d5271d9120a1
Евгений Сапрунов <sapra10062018@gmail.com>
2024-08-30 16:23:35 +0300

    added fields to the entity class.

--- src/main/java/com/example/newsservice2/user/model/UserEntity.java
+++ src/main/java/com/example/newsservice2/user/model/UserEntity.java
@@ -1,11 +1,17 @@
 package com.example.newsservice2.user.model;
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
 @Table(name = "usr")
+@Getter
+@Setter
 public class UserEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    private String email;
+    private String nickName;
 }
