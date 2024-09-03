c51adcffc2ca7421b9298d83d2c996b45492a98f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-03 16:42:00 +0300

    Refactor: unnecessary annotations have been removed. Added validation tests.

--- src/test/java/com/example/newsservice2/user/input/web/v1/controller/UserControllerTest.java
+++ src/test/java/com/example/newsservice2/user/input/web/v1/controller/UserControllerTest.java
@@ -1,26 +1,25 @@
 package com.example.newsservice2.user.input.web.v1.controller;
 
+import com.example.newsservice2.category.input.web.v1.model.CategoryFilter;
 import com.example.newsservice2.config.AbstractIntegrationTest;
 import com.example.newsservice2.testUtils.StringTestUtils;
 import com.example.newsservice2.user.input.web.v1.model.UserFilter;
 import com.example.newsservice2.user.input.web.v1.model.UserPayload;
 import com.example.newsservice2.user.model.UserEntity;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import jakarta.transaction.Transactional;
 import net.javacrumbs.jsonunit.JsonAssert;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.ImportAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.core.AutoConfigureCache;
 import org.springframework.boot.test.autoconfigure.filter.TypeExcludeFilters;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTypeExcludeFilter;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
@@ -33,9 +32,6 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @TypeExcludeFilters(DataJpaTypeExcludeFilter.class)
-@Transactional
-@AutoConfigureCache
-@ImportAutoConfiguration
 @AutoConfigureMockMvc
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 @AutoConfigureTestEntityManager
@@ -53,6 +49,29 @@ class UserControllerTest extends AbstractIntegrationTest {
         UserFilter f2 = new UserFilter();
         f2.setPageNumber(1);
         f2.setPageSize(3);
+        return Stream.of(
+                Arguments.of(f1),
+                Arguments.of(f2)
+        );
+    }
+
+    public static Stream<Arguments> getNegativePageNumber() {
+        CategoryFilter f1 = new CategoryFilter();
+        f1.setPageSize(1);
+        f1.setPageNumber(-1);
+
+        return Stream.of(
+                Arguments.of(f1)
+        );
+    }
+
+    public static Stream<Arguments> getNegativePageSize() {
+        CategoryFilter f1 = new CategoryFilter();
+        f1.setPageSize(-1);
+        f1.setPageNumber(1);
+        CategoryFilter f2 = new CategoryFilter();
+        f2.setPageSize(0);
+        f2.setPageNumber(1);
         return Stream.of(
                 Arguments.of(f1),
                 Arguments.of(f2)
@@ -125,5 +144,45 @@ class UserControllerTest extends AbstractIntegrationTest {
     void whenDelete_thanReturnNoContent() throws Exception {
         mvc.perform(delete("/api/v1/user/{id}", 1))
                 .andExpect(status().isNoContent());
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNegativePageNumber")
+    void whenPageNumberIsNegative_thenReturnError(CategoryFilter filter) throws Exception {
+        MockHttpServletResponse response = mvc.perform(
+                        get("/api/v1/user")
+                                .param("pageNumber", String.valueOf(filter.getPageNumber()))
+                                .param("pageSize", String.valueOf(filter.getPageSize())))
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse();
+
+        response.setCharacterEncoding("UTF-8");
+
+        String actual = response.getContentAsString();
+
+        String expected = StringTestUtils.
+                readStringFromResources("/response/user/web/v1/error_pageNumber_response.json");
+
+        JsonAssert.assertJsonEquals(expected, actual);
+    }
+
+    @ParameterizedTest
+    @MethodSource("getNegativePageSize")
+    void whenPageSizeIsNegative_orIsZero_thenReturnError(CategoryFilter filter) throws Exception {
+        MockHttpServletResponse response = mvc.perform(
+                        get("/api/v1/user")
+                                .param("pageNumber", String.valueOf(filter.getPageNumber()))
+                                .param("pageSize", String.valueOf(filter.getPageSize())))
+                .andExpect(status().isBadRequest())
+                .andReturn().getResponse();
+
+        response.setCharacterEncoding("UTF-8");
+
+        String actual = response.getContentAsString();
+
+        String expected = StringTestUtils.
+                readStringFromResources("/response/user/web/v1/error_pageSize_response.json");
+
+        JsonAssert.assertJsonEquals(expected, actual);
     }
 }
