28826e8b59883fc91de1e8d5eea1fcdc726794ae
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-03 16:09:34 +0300

    A wrapper class has been created for the user id

---
+++ src/main/java/com/example/newsservice2/user/input/web/v1/model/UserId.java
@@ -0,0 +1,12 @@
+package com.example.newsservice2.user.input.web.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserId {
+    private Long id;
+}
