dc75995e6368327416d77302441b0d5130ac23c9
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-03 16:46:57 +0300

    Pagination errors have been processed using ControllerAdvice

---
+++ src/main/java/com/example/newsservice2/user/input/web/v1/controller/UserRestControllerAdvice.java
@@ -0,0 +1,35 @@
+package com.example.newsservice2.user.input.web.v1.controller;
+
+import com.example.newsservice2.user.input.web.v1.model.UserErrorResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice(basePackages = "com.example.newsservice2.user.input.web.v1.controller")
+public class UserRestControllerAdvice {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<UserErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException ex){
+        var errorResponse = new UserErrorResponse();
+
+        List<String> errorList = new ArrayList<>();
+        for (ObjectError objectError : ex.getBindingResult().getAllErrors()) {
+            String defaultMessage = objectError.getDefaultMessage();
+            errorList.add(defaultMessage);
+        }
+
+        String errorMessage = String.join("; ", errorList);
+
+        errorResponse.setMessage(errorMessage);
+
+        return ResponseEntity.status(BAD_REQUEST).body(errorResponse);
+    }
+
+}
--- src/main/java/com/example/newsservice2/user/input/web/v1/model/UserFilter.java
+++ src/main/java/com/example/newsservice2/user/input/web/v1/model/UserFilter.java
@@ -1,5 +1,7 @@
 package com.example.newsservice2.user.input.web.v1.model;
 
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.PositiveOrZero;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -8,6 +10,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserFilter {
+    @PositiveOrZero(message = "Номер страницы должен быть положителен!")
     private int pageNumber;
+    @Positive(message = "Размер страницы должен быть положителен!")
     private int pageSize;
 }
