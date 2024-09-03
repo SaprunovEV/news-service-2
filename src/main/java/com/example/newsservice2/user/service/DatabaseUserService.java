3ed3de3a136859012da245eb7a27dd8bf22deb37
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-03 14:48:09 +0300

    A method has been created that returns a page with users

---
+++ src/main/java/com/example/newsservice2/user/service/DatabaseUserService.java
@@ -0,0 +1,23 @@
+package com.example.newsservice2.user.service;
+
+import com.example.newsservice2.user.input.web.v1.model.UserFilter;
+import com.example.newsservice2.user.model.UserEntity;
+import com.example.newsservice2.user.output.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class DatabaseUserService implements UserService {
+    private final UserRepository repository;
+
+    @Override
+    public List<UserEntity> findAll(UserFilter filter) {
+        Page<UserEntity> page = repository.findAll(PageRequest.of(filter.getPageNumber(), filter.getPageSize()));
+        return page.getContent();
+    }
+}
---
+++ src/main/java/com/example/newsservice2/user/service/UserService.java
@@ -0,0 +1,10 @@
+package com.example.newsservice2.user.service;
+
+import com.example.newsservice2.user.input.web.v1.model.UserFilter;
+import com.example.newsservice2.user.model.UserEntity;
+
+import java.util.List;
+
+public interface UserService {
+    List<UserEntity> findAll(UserFilter filter);
+}
