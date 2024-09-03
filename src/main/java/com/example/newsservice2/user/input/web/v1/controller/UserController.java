00ccc6e2c299c6023374f35d31e117f7a135f17b
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-03 14:50:26 +0300

    A controller for user API

--- src/main/java/com/example/newsservice2/user/input/web/v1/controller/UserController.java
+++ src/main/java/com/example/newsservice2/user/input/web/v1/controller/UserController.java
@@ -1,12 +1,24 @@
 package com.example.newsservice2.user.input.web.v1.controller;
 
+import com.example.newsservice2.user.input.web.v1.model.UserFilter;
+import com.example.newsservice2.user.input.web.v1.model.UserListResponse;
+import com.example.newsservice2.user.mapper.UserMapper;
+import com.example.newsservice2.user.service.UserService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @RestController
 @RequestMapping("/api/v1/user")
-@RestControllerAdvice
+@RequiredArgsConstructor
 public class UserController {
-
+    private final UserService service;
+    private final UserMapper mapper;
+    @GetMapping
+    public ResponseEntity<UserListResponse> getAllUser(@Valid UserFilter filter) {
+        return ResponseEntity.ok(mapper.listEntitiesToResponseList(service.findAll(filter)));
+    }
 }
