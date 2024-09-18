96339445a4b6f4af6c12e146c8da6a10d193e20b
Евгений Сапрунов <sapra10062018@gmail.com>
2024-09-18 16:23:41 +0300

    Added test data builder of the comments.

---
+++ src/test/java/com/example/newsservice2/testUtils/CommentTestDataBuilder.java
@@ -0,0 +1,32 @@
+package com.example.newsservice2.testUtils;
+
+import com.example.newsservice2.news.model.CommentEntity;
+import com.example.newsservice2.news.model.NewsEntity;
+
+public class CommentTestDataBuilder implements TestDataBuilder<CommentEntity> {
+    private TestDataBuilder<NewsEntity> news = NewsEntity::new;
+
+    private CommentTestDataBuilder() {
+    }
+
+    private CommentTestDataBuilder(TestDataBuilder<NewsEntity> news) {
+        this.news = news;
+    }
+
+    public static CommentTestDataBuilder aComment() {
+        return new CommentTestDataBuilder();
+    }
+
+    public CommentTestDataBuilder withNews(TestDataBuilder<NewsEntity> news) {
+        return new CommentTestDataBuilder(news);
+    }
+
+    @Override
+    public CommentEntity build() {
+        CommentEntity result = new CommentEntity();
+        NewsEntity newsEntity = news.build();
+        newsEntity.getComment().add(result);
+        result.setNews(newsEntity);
+        return result;
+    }
+}
