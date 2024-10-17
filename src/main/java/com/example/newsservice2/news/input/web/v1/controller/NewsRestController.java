7b7021fadba21b28da125c0595e2a68c0d42e35f
Евгений Сапрунов <sapra10062018@gmail.com>
2024-10-17 15:46:27 +0300

    Added a search method for the news page.

--- src/main/java/com/example/newsservice2/news/input/web/v1/controller/NewsRestController.java
+++ src/main/java/com/example/newsservice2/news/input/web/v1/controller/NewsRestController.java
@@ -1,6 +1,13 @@
 package com.example.newsservice2.news.input.web.v1.controller;
 
+import com.example.newsservice2.news.input.web.v1.mapper.NewsMapper;
+import com.example.newsservice2.news.input.web.v1.model.NewsFilter;
+import com.example.newsservice2.news.input.web.v1.model.NewsPageResponse;
+import com.example.newsservice2.news.service.NewsService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,4 +15,13 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/news")
 @RequiredArgsConstructor
 public class NewsRestController {
+    private final NewsService service;
+    private final NewsMapper mapper;
+
+    @GetMapping
+    public ResponseEntity<NewsPageResponse> getPage(@Valid NewsFilter filter) {
+        return ResponseEntity.ok(
+                mapper.listEntityToPageResponse(
+                        service.getPageNews(filter.getPageNumber(), filter.getPageSize())));
+    }
 }
